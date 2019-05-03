5b998bb4dbe080cee2b9899fc8d1a5a16858e7fb
The Singer <kayla312@harbornet.com>
2019-05-03 13:49:57 -0700

    adjusting the commissary schema and inout products

--- commissary.sql
+++ commissary.sql
@@ -1,21 +1,30 @@
-DROP DATABASE IF EXISTS commissaryDB;
-CREATE database commissaryDB;
+DROP DATABASE IF EXISTS commissary_db;
+CREATE database commissary_db;
 
-USE commissaryDB;
+USE commissary_db;
 
 CREATE TABLE products (
-  ID INT AUTO_INCREMENT,
-  product_name VARCHAR(100) NULL,
-  department_name VARCHAR(100) NULL,
-  price DECIMAL(10,2) NULL,
-  stock_quantity INT (10) NULL,
-  sold INT(10) DEFAULT 0,
+  ID INT AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100)NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT (10) NOT NULL,
   PRIMARY KEY (ID)
 );
 
-ALTER TABLE products,
-DROP index;
+-- ALTER TABLE products,
+-- DROP index;
 SELECT * FROM  products;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES("Trail Mix", "Snacks", 1.50, 100),("Sun Screen", "First Aid", 9.89, 35), ("Water Bottle", "Essentials", 15.00, 40),("Pencils", "Utensils", 0.50, 500), ("Cards Home", "Utensils", 1.50, 130), ("Back Pack", "Essentials", 35.55, 20),("Bandages", "First Aid", 0.10, 1000), ("Tweezers", "First Aid", 2.00, 20),("Hairl Ties", "Utensils", 2.00, 15), ("Stickers", "Essentials", 0.10, 5000);
+VALUES
+ ("Trail Mix", "Snacks", 1.50, 100),
+ ("Sun Screen", "First Aid", 9.89, 35),
+ ("Water Bottle", "Essentials", 15.00, 40),
+ ("Pencils", "Utensils", 0.50, 500), 
+ ("Cards Home", "Utensils", 1.50, 130), 
+ ("Back Pack", "Essentials", 35.55, 20),
+ ("Bandages", "First Aid", 0.10, 1000), 
+ ("Tweezers", "First Aid", 2.00, 20),
+ ("Hairl Ties", "Utensils", 2.00, 15), 
+ ("Stickers", "Essentials", 0.10, 5000);
